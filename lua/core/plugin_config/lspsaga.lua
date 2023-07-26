3ee41dc1b8cfc011fa69b48f165fbe550e0fea0f
1skar05 <liamlee7797@gmail.com>
2023-07-26 15:19:00 +0700

    center telescope_file_browser popup

--- lua/core/plugin_config/lspsaga.lua
+++ lua/core/plugin_config/lspsaga.lua
@@ -1,29 +1,31 @@
-local status_ok, saga = pcall(require, 'lspsaga')
-if not status_ok then return end
+local status_ok, saga = pcall(require, "lspsaga")
+if not status_ok then
+	return
+end
 
-saga.setup {
--- show outline
-	  ui = {
-    border = 'rounded',
-  },
-  symbol_in_winbar = {
-    enable = false
-  },
-  lightbulb = {
-    enable = false
-  },
-outline = {
-	layout = "float",
-}, 
-}
+saga.setup({
+	-- show outline
+	ui = {
+		border = "rounded",
+	},
+	symbol_in_winbar = {
+		enable = false,
+	},
+	lightbulb = {
+		enable = false,
+	},
+	outline = {
+		layout = "float",
+	},
+})
 
-local opts = {noremap = true, silent = true}
-vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<cr>', opts)
-vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<cr>', opts)
-vim.keymap.set('n', 'gf', '<Cmd>Lspsaga lsp_finder<cr>', opts)
-vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<cr>', opts)
-vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<cr>', {})
-vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<cr>', opts)
-vim.keymap.set('n', '<leader>ol', '<Cmd>Lspsaga outline<cr>', opts)
-vim.keymap.set('n', '<A-d>', '<Cmd>Lspsaga term_toggle<cr>', opts)
-vim.keymap.set('t', '<A-d>', '<Cmd>Lspsaga term_toggle<cr>', opts)
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<cr>", opts)
+vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<cr>", opts)
+vim.keymap.set("n", "gf", "<Cmd>Lspsaga finder<cr>", opts)
+vim.keymap.set("i", "<C-k>", "<Cmd>Lspsaga signature_help<cr>", opts)
+vim.keymap.set("n", "gp", "<Cmd>Lspsaga peek_definition<cr>", {})
+vim.keymap.set("n", "gr", "<Cmd>Lspsaga rename<cr>", opts)
+vim.keymap.set("n", "<leader>ol", "<Cmd>Lspsaga outline<cr>", opts)
+vim.keymap.set("n", "<A-d>", "<Cmd>Lspsaga term_toggle<cr>", opts)
+vim.keymap.set("t", "<A-d>", "<Cmd>Lspsaga term_toggle<cr>", opts)
